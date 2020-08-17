ed257d1a9faa591afb054433bd32eac2cf7a2181
Omer Marquez <omer.marquez54@gmail.com>
2020-08-16 22:37:07 -0400

    Basic Setup

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+//Global CSS
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'components/css/utilities.css';
+
+export default function MyApp({ Component, pageProps }) {
+
+  return (
+    <>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
