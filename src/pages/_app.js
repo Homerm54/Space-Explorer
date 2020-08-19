95ac3c854baad760f2e8928edcfff1f7f3844585
Omer Marquez <omer.marquez54@gmail.com>
2020-08-19 18:25:44 -0400

    NProgress Added and Modified

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,34 @@
 import Head from 'next/head';
 
+import Router from 'next/router';
+import NProgress from 'nprogress/nprogress';
+
 //Global CSS
+import 'nprogress/nprogress.css'
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import 'components/css/fonts.css';
 import 'components/css/utilities.css';
 import 'components/css/palette.css';
 
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {// Fallback
+  NProgress.done();
+};
+
 export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 export default function Test(){
   return(
-    <h1>Hola</h1>
+    <>
+      <h1>Hola</h1>
+      <Link href="test"><a>Go next page</a></Link>
+    </>
   )
 }
