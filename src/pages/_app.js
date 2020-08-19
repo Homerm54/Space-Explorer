d759c62ee3f21fdf1d8e6835cb96a8cf16029e79
Omer Marquez <omer.marquez54@gmail.com>
2020-08-19 16:44:33 -0400

    Fonts Added to Project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 //Global CSS
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'components/css/fonts.css';
 import 'components/css/utilities.css';
 import 'components/css/palette.css';
 
