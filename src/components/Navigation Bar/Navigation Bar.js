633f11d8d33520319dfbaae71ca32d8b35d80255
Omer Marquez <omer.marquez54@gmail.com>
2020-08-19 22:06:10 -0400

    Navbar Toggle Added

--- src/components/Navigation Bar/Navigation Bar.js
+++ src/components/Navigation Bar/Navigation Bar.js
@@ -8,6 +8,7 @@ import styles from './style.module.css';
 function Navbar() {
 
   const [navLinksDisplay, setNavLinksDisplay] = useState(false);
+  const toggleState = () => (setNavLinksDisplay(!navLinksDisplay));
 
   return (
     <section className={`container ${styles.container}`}>
@@ -15,12 +16,13 @@ function Navbar() {
       <div className={`row ${styles['nav-header']}`}>
         <h2 className={`col h3 ${styles.title}`}>Space Explorer</h2>
         <span className={`col-auto h4 ${styles.toggleButton}`} role="button"
-          onClick={() => (setNavLinksDisplay(!navLinksDisplay))}>
+          onClick={toggleState}>
           {navLinksDisplay ? <FontAwesomeIcon icon="times" className="text-blue" /> : <FontAwesomeIcon icon="bars" />}
         </span>
       </div>
 
-      <div className={`row ${styles['nav-links-container']} ${navLinksDisplay && styles['show-navContainer']}`}>
+      <nav className={`row ${styles['nav-links-container']} ${navLinksDisplay && styles['show-navContainer']}`}
+        onClick={toggleState}>
         <ul className={`${styles['nav-links-list']}`}>
           <li className={styles['nav-item']}>
             <Link href="/">
@@ -53,7 +55,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
-      </div>
+      </nav>
 
     </section>
   )
