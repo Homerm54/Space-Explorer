c297f989916960a50fe601d42305ff691b8534cc
Omer Marquez <omer.marquez54@gmail.com>
2020-08-20 18:21:46 -0400

    Copyright Note

--- src/components/Landpage/Container/Container.js
+++ src/components/Landpage/Container/Container.js
@@ -3,7 +3,13 @@ import styles from './style.module.css';
 function Container({ children }) {
   return (
     <main className={styles.container}>
-      <section className={`container`}>
+      <div className={styles.copyright}>
+        Copyright <br />
+        <a className={styles.a} href="https://www.flickr.com/people/bluemoonlife/" target="_blank">
+          Hirofumi Okubo
+          </a>
+      </div>
+      <section className={`container ${styles.section}`}>
         {children}
       </section>
     </main>
