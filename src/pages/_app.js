9472677302118f66144cdb498568560288bc171e
Omer Marquez <omer.marquez54@gmail.com>
2020-08-22 12:00:04 -0400

    SEO Part 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,9 +8,9 @@ import Navbar from 'components/Navigation Bar/Navigation Bar';
 import 'components/Fontawesome Icon Library/Library';
 
 //Global CSS
-import 'nprogress/nprogress.css'
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'components/nprogress.css'
 import 'components/css/fonts.css';
 import 'components/css/utilities.css';
 import 'components/css/palette.css';
@@ -33,10 +33,23 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
+        <meta name="keywords" content="NASA, Nasa, Space, Galaxy, Explore, Telescope, Galaxy Exploration, Universe, Mars, SpaceX, Mars Rover, Space Images, Space Videos, PWA, Progressive Web App, Space Explorer, Web App, Awesome Site, Explore Universe, Nasa API" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Omer Marquez" />
+        <meta name="robots" content="index, follow" />
+
+        <link rel="icon" href="images/Icons/favicon.ico" />
+        <link rel="apple-touch-icon" href="images/Icons/apple-touch-icon.png" />
+        {/**<link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
       </Head>
       <Navbar />
       <Component {...pageProps} />
+      <noscript>
+        You need to enable JavaScript to run the features of this app.
+      </noscript>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 import Landpage from 'components/Landpage/Main';
 
 export default function Home(){
   return(
-    <Landpage />
+    <>
+      <Head>
+        <meta name="description" content="Explore the Cosmos from your Home. Access to Nasa's API, learn and discover galaxies, exoplanets, and more." />
+        <title>
+          Space Explorer
+        </title>
+      </Head>
+      <Landpage />
+    </>
   )
 }
